1cc413c2498c6b93b0de3bf19669fe65037d3956
XueWang <13452916191@163.com>
2017-08-25 20:22:13 +0800

    delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return render_template('index.html',title='<h1>Hello!</h1>')
-
-if __name__ == "__main__":
-	app.run(debug=True)
